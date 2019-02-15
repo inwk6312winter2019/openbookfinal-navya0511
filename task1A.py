389ccd2edcdade63f6b3dddf4c3155d53ec7c9e7
[navya0511] <[navyasree.sadanala@dal.ca]>
2019-02-15 10:41:14 -0800

    task 1A.4

--- task1A.py
+++ task1A.py
@@ -39,3 +39,18 @@ def sorted_words(file):
     print(result)
 
 sorted_words(file1)
+
+def character_word_count(file):
+    hist=dict()
+    for line in file:
+        line=line.lower()
+        line=line.strip()
+        line=line.split()
+        for word in line:
+            if word not in hist:
+                hist[word]=1
+            else:
+                hist[word]+=1
+    print(hist)
+
+character_word_count(file1)
